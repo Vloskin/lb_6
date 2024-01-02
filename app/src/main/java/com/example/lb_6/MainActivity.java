8dbc737526daa7a2589f22c9698d0392c4990559
Vlosk <Vloskin@outlook.com>
2024-01-02 20:31:36 +0300

    Luka

--- app/src/main/java/com/example/lb_6/MainActivity.java
+++ app/src/main/java/com/example/lb_6/MainActivity.java
@@ -2,29 +2,69 @@ package com.example.lb_6;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.drawable.AnimationDrawable;
-import android.view.animation.AnimationUtils;
+import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
-
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    private AnimationDrawable mAnimationDrawable;
+    ImageView imageView;
+    ImageView imageView2;
+    boolean flag = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*ImageView animationView = findViewById(R.id.animationView);
-        animationView.setBackgroundResource(R.drawable.my_animation);
-        AnimationDrawable animationDrawable = (AnimationDrawable) animationView.getBackground();
-        animationDrawable.start();*/
 
 
-        // Анимация падения яблока
-        ImageView appleImageView = findViewById(R.id.appleImageView);
-        Animation fallAnimation = AnimationUtils.loadAnimation(this, R.anim.fall_animation);
-        appleImageView.startAnimation(fallAnimation);
+        final Button btnfirst = findViewById(R.id.firsAnim);
+        final Button btnsecond = findViewById(R.id.secondAnim);
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.second_anim);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setBackgroundResource(R.drawable.first_anim);
+        mAnimationDrawable = (AnimationDrawable) imageView.getBackground();
+        imageView2 = (ImageView) findViewById(R.id.imageView2);
+        imageView2.setBackgroundResource(R.drawable.babochka);
+        imageView.setVisibility(View.INVISIBLE);
+        imageView2.setVisibility(View.INVISIBLE);
+        btnfirst.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick (View v){
+
+                if(flag)
+                {
+                    flag =!flag;
+                    imageView.setVisibility(View.VISIBLE);
+                    mAnimationDrawable.start();
+
+                }
+                else if(!flag)
+                {
+                    flag =!flag;
+                    imageView.setVisibility(View.INVISIBLE);
+                    mAnimationDrawable.stop();
+                }
+
+            }
+        });
+
+        btnsecond.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick (View v){
+
+                imageView.setVisibility(View.INVISIBLE);
+                imageView2.startAnimation(animation);
+
+            }
+        });
+
     }
+
+
+
 }
